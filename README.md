77e1fd00752674029e8f736313faa0b812b2ed00
Shrey Vishen <shrey.vishen@gmail.com>
2024-10-25 18:19:07 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,28 @@
+Next Steps:
+- Implement diffusion models and diffusion transformers training and sampling
+
+Models:
+- GANs (Try different types of GANs - DCGAN, CGAN, WGAN)
+- VAEs (Sampling from N in the latent space)
+- Diffusion Models (Denosing, Latent, Non-Latent)
+- Diffusion Transformer (Latent, Non-Latent)
+
+Techniques to try:
+- Basic: Train each model on the given datasets, conditional generation, and then generate each until there are ~8000 of each, except NonDemented
+    - Pros: Easy to train, not much compute
+    - Cons: Same issue of lack of data for certain classes
+- Weighted training: Train each model as basic, but use a weighted loss to give higher priority to the smaller classes
+    - Pros: Solve the issue of lack of data
+    - Cons: Might cause the opposite issue, will have to try many weight functions
+- Dataset Repetition: Repeat the smaller datasets a few times so the model sees all equally
+    - Pros: Solve the issue of lack of data
+    - Cons: Might cause the model to generate the same type of data for smaller classes
+- Dataset Augmentation: Augment the smaller datasets a few times so the model sees all equally
+    - Pros: Solve the issue of lack of data
+    - Cons: Might cause unnatural generation of data, depends heavily on augmentation
+- Unconditional Training: Train a unique model for each class, and then augment using each model
+    - Pros: Solves all issues above, as classes are treated as independent
+    - Cons: Lots of compute needed to train 4 seperate Models
+- Unconditional + Average of Models: Train the 4 models, and then take their weighted average and finetune the new model
+    - Pros: While augmentating, saves compute and storage
+    - Cons: Still compute needed to train 4 different models
---
+++ src/utils.py
@@ -0,0 +1,81 @@
+import torch
+from torch import nn
+from torch.nn import functional as F
+import math
+
+class SelfAttention(nn.Module):
+    def __init__(self, n_heads, d_embed, in_proj_bias=True, out_proj_bias=True):
+        super().__init__()
+        self.in_proj = nn.Linear(d_embed, 3 * d_embed, bias=in_proj_bias)
+        self.out_proj = nn.Linear(d_embed, d_embed, bias=out_proj_bias)
+        self.n_heads = n_heads
+        self.d_head = d_embed // n_heads
+        
+    def forward(self, x, causal_mask=False):
+        input_shape = x.shape 
+        
+        batch_size, sequence_length, d_embed = input_shape 
+        interim_shape = (batch_size, sequence_length, self.n_heads, self.d_head) 
+        q, k, v = self.in_proj(x).chunk(3, dim=-1)
+        
+        q = q.view(interim_shape).transpose(1, 2)
+        k = k.view(interim_shape).transpose(1, 2)
+        v = v.view(interim_shape).transpose(1, 2)
+
+        weight = q @ k.transpose(-1, -2)
+        if causal_mask:
+            mask = torch.ones_like(weight, dtype=torch.bool).triu(1) 
+            weight.masked_fill_(mask, -torch.inf) 
+        
+        weight /= math.sqrt(self.d_head) 
+        weight = F.softmax(weight, dim=-1) 
+
+        output = weight @ v
+        output = output.transpose(1, 2).reshape(input_shape)  
+        output = self.out_proj(output) 
+        
+        return output
+    
+class AttentionBlock(nn.Module):
+    def __init__(self, channels):
+        super().__init__()
+        self.groupnorm = nn.GroupNorm(32, channels)
+        self.attention = SelfAttention(1, channels)
+    def forward(self, x):
+        residue = x 
+        x = self.groupnorm(x)
+        n, c, h, w = x.shape
+        
+        x = x.view((n, c, h * w)).transpose(-1, -2)
+        x = self.attention(x)
+        x = x.transpose(-1, -2)
+        
+        x = x.view((n, c, h, w))
+        x += residue
+        return x
+    
+class ResidualBlock(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super().__init__()
+        self.groupnorm_1 = nn.GroupNorm(32, in_channels)
+        self.conv_1 = nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1)
+
+        self.groupnorm_2 = nn.GroupNorm(32, out_channels)
+        self.conv_2 = nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1)
+
+        if in_channels == out_channels:
+            self.residual_layer = nn.Identity()
+        else:
+            self.residual_layer = nn.Conv2d(in_channels, out_channels, kernel_size=1, padding=0)
+
+    def forward(self, x):
+        residue = x
+        x = self.groupnorm_1(x)
+        x = F.silu(x)
+        
+        x = self.conv_1(x)
+        x = self.groupnorm_2(x)
+        x = F.silu(x)
+        
+        x = self.conv_2(x)
+        return x + self.residual_layer(residue)
